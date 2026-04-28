e4601499d57cc33689c8654b89db77e696a860d7
ckpnm <kxwtxnkx@gmail.com>
2026-04-28 17:01:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 REPO_URL="https://github.com/ckpnm/aio_gentle_utility.git"
 INSTALL_DIR="/opt/aio_gentle"
 
@@ -16,13 +17,20 @@ apt-get install -y -qq git curl jq >/dev/null 2>&1
 
 if [ -d "$INSTALL_DIR" ]; then
     echo -e "\e[90mОбновление существующей установки...\e[0m"
-    cd "$INSTALL_DIR" && git pull origin main >/dev/null 2>&1
+    cd "$INSTALL_DIR" && git pull origin main
 else
     echo -e "\e[90mКлонирование репозитория...\e[0m"
-    git clone "$REPO_URL" "$INSTALL_DIR" >/dev/null 2>&1
+    git clone "$REPO_URL" "$INSTALL_DIR"
+fi
+
+if [ ! -f "$INSTALL_DIR/main.sh" ]; then
+    echo -e "\e[1;31m[ ОШИБКА ] Файл main.sh не найден! Убедись, что файлы лежат в корне репозитория.\e[0m"
+    exit 1
 fi
 
 chmod +x "$INSTALL_DIR/main.sh"
 ln -sf "$INSTALL_DIR/main.sh" /usr/local/bin/aio_gentle
 
-echo -e "\e[32m[ ИНФО ] Утилита установлена! Введи команду: \e[1m\e[97maio_gentle\e[0m"
+echo -e "\e[32m[ ИНФО ] Утилита установлена! Запускаем...\e[0m"
+sleep 1
+exec aio_gentle
